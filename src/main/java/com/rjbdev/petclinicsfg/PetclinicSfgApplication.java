9470d13a2f7790e6d9535219663eee500e754707
rjboy <rjboyce@teksavvy.com>
2018-11-07 17:34:34 -0500

    Initialized Project

---
+++ src/main/java/com/rjbdev/petclinicsfg/PetclinicSfgApplication.java
@@ -0,0 +1,12 @@
+package com.rjbdev.petclinicsfg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclinicSfgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetclinicSfgApplication.class, args);
+    }
+}
