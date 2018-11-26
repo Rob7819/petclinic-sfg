3adf1f56524e53545299b79451f7ee442a51b570
rjboy <rjboyce@teksavvy.com>
2018-11-26 16:24:46 -0500

    Implemented Owner, Pet and PetType classes as JPA.  Defined inheritance implementation for Person class.  Closes #36.

--- pet-clinic-data/src/main/java/com/rjbdev/petclinicsfg/model/PetType.java
+++ pet-clinic-data/src/main/java/com/rjbdev/petclinicsfg/model/PetType.java
@@ -1,7 +1,15 @@
 package com.rjbdev.petclinicsfg.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 //RJB
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
 
     public String getName() {
