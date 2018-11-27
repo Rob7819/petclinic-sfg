4d2dbd6aab763a1094c842242cea0e847e580dfc
rjboy <rjboyce@teksavvy.com>
2018-11-27 18:28:51 -0500

    Created Spring Data JPA Service for Visit.  Closes #47.

--- pet-clinic-web/src/main/java/com/rjbdev/petclinicsfg/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/rjbdev/petclinicsfg/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package com.rjbdev.petclinicsfg.bootstrap;
 
 import com.rjbdev.petclinicsfg.model.*;
-import com.rjbdev.petclinicsfg.services.OwnerService;
-import com.rjbdev.petclinicsfg.services.PetTypeService;
-import com.rjbdev.petclinicsfg.services.SpecialtyService;
-import com.rjbdev.petclinicsfg.services.VetService;
+import com.rjbdev.petclinicsfg.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,13 +14,15 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialtyService specialtyService;
+    private final VisitService visitService;
 
     //@Autowired is not needed in Spring 5.0+
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialtyService = specialtyService;
+        this.visitService = visitService;
     }
 
 
@@ -78,6 +77,13 @@ public class DataLoader implements CommandLineRunner {
         owner2.getPets().add(fionasPet);
 
         ownerService.save(owner2);
+
+        Visit catVisit = new Visit();
+        catVisit.setPet(fionasPet);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Sneezy kitty");
+
+        visitService.save(catVisit);
 
         System.out.println("Loaded owners....");
 
